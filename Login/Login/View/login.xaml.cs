1aaf727c5e703918621746bc08515ddc9518e41f
thiag0-dot <thiagodeginofilho@gmail.com>
2022-08-25 15:44:56 -0300

    sexto commit

--- Login/Login/View/login.xaml.cs
+++ Login/Login/View/login.xaml.cs
@@ -12,14 +12,35 @@ namespace Login.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class login : ContentPage
     {
+        App PropriedadesApp;
+
         public login()
         {
             InitializeComponent();
+            PropriedadesApp = (App)Application.Current;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                string email = txt_email.Text;
+                string senha = txt_senha.Text;
 
+                if (PropriedadesApp.list_usuarios.Any(i => (i.Email == email && i.senha == senha)))
+                {
+                    App.Current.Properties.Add("usuario_logado", email);
+                    App.Current.MainPage = new protegida();
+                }
+                else
+                {
+                    throw new Exception("Dados incorretos, tente novamente.");
+                    
+                }
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 }
