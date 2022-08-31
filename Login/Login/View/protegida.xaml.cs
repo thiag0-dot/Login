14d9c396f5eec3d88f1782afaaa0822ea5d6177d
thiag0-dot <thiagodeginofilho@gmail.com>
2022-08-31 15:46:21 -0300

    setimo commit-resolver bug

--- Login/Login/View/protegida.xaml.cs
+++ Login/Login/View/protegida.xaml.cs
@@ -6,19 +6,46 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Login.Model;
 
 namespace Login.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class protegida : ContentPage
     {
+        App PropriedadesApp;
         public protegida()
         {
             InitializeComponent();
+
+            Set_Boas_Vindas();
+            PropriedadesApp = (App)Application.Current;
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void Set_Boas_Vindas()
         {
+            string email_login = PropriedadesApp.Properties["usuario_logado"].ToString();
+
+            Dados usuario_logado = PropriedadesApp.list_usuarios.FirstOrDefault(i => i.Email == email_login);
+
+            lbl_boasvindas.Text = "Bem-vindo (a) " + usuario_logado.Nome;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                bool confirm = await DisplayAlert("Tem certeza?", "Desconectar sua conta?", "Sim", "Não");
+
+                if (confirm)
+                {
+                    App.Current.Properties.Remove("usuario_logado");
+                    App.Current.MainPage = new login();
+                }
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
 
         }
     }
